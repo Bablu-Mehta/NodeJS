3180a664b3682b0568dbb93a224d7a2b5b398675
Bablu Mehta <babloomehta895012@gmail.com>
2025-01-18 19:07:23 +0530

    parsing request bodies

--- understanding the basics/app.js
+++ understanding the basics/app.js
@@ -1,22 +1,32 @@
 const http = require("http");
-const fs = require('fs');
+const fs = require("fs");
 
 function rqListener(req, res) {
   //   console.log(req.url, req.method, req.headers);
   const url = req.url;
   const method = req.method;
-  if(url === '/'){
+  if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
-    res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>send</button></form></body>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>send</button></form></body>"
+    );
     res.write("</html>");
     return res.end();
   }
 
-  if(url === '/message' && method === 'POST'){
-    fs.writeFileSync("message.txt", "Dummy");
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
     res.statusCode = 302;
-    res.setHeader("Location", '/');
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
